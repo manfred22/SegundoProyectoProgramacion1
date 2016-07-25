f4bb897a91705cfdafb1a14652d7f6d15060df96
manfred22 <martineli@ucatolica.ac.cr>
2016-07-24 20:12:06 -0600

    Creación de las primeras clases a ver
    Creación de algunas clases a usar y parte del documentariado.

--- Proyecto2Programacion1/src/Buscaminas/Menu.java
+++ Proyecto2Programacion1/src/Buscaminas/Menu.java
@@ -5,18 +5,11 @@
  */
 //Paquetes
 package Buscaminas;
+//Importes
 /**
  **
  ** @author Manfred Martineli Rojas
  ** @date 2016-07-24
  **/
-public class Principal {
-    /**
-     * @param args the command line arguments
-     **/
-    public static void main(String[] args) {
-        // TODO code application logic here
-    //Intastancia de la clase Menu
-        Menu m = new Menu();
-    }//Fin del main{}
-}//Fin de la clase {}
+public class Menu {
+}//Fin de la clase menu {}
---
+++ Proyecto2Programacion1/src/Estructura/Buscaminas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Paquetes
+package Estructura;
+//Importes
+/**
+ **
+ ** @author Manfred Martineli Rojas
+ ** @date 2016-07-24
+ **/
+public class Buscaminas {
+}//Fin de la clase Busacaminas {}
